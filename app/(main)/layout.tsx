b2984a5ed84ccfc59207a3fd29f15ceffb35298d
Taimoor Imran <taimoorimran9@gmail.com>
2024-07-12 16:32:37 +0500

    (main) grouping

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+const Layout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return (
+        <main>
+            {children}
+        </main>
+    )
+}
+
+export default Layout;
