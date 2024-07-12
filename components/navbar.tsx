a13bcc8389bfe1099636141f250601c5d574ca38
Taimoor Imran <taimoorimran9@gmail.com>
2024-07-12 19:25:01 +0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,36 @@
+'use client';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Navbar = () => {
+    const pathname = usePathname();
     return (
         <nav className="navbar">
             <ul className="navbar-list">
                 <li className="navbar-item">
-                    <button className="navbar-link  active" data-nav-link>About</button>
+                    <Link href="/">
+                        <button className={`navbar-link ${pathname == '/' && 'active'}`}>About</button>
+                    </Link>
                 </li>
                 <li className="navbar-item">
-                    <button className="navbar-link" data-nav-link>Resume</button>
+                    <Link href="/resume">
+                        <button className={`navbar-link ${pathname == '/resume' && 'active'}`}>Resume</button>
+                    </Link>
                 </li>
                 <li className="navbar-item">
-                    <button className="navbar-link" data-nav-link>Portfolio</button>
+                    <Link href="/portfolio">
+                        <button className={`navbar-link ${pathname == '/portfolio' && 'active'}`}>Portfolio</button>
+                    </Link>
                 </li>
                 <li className="navbar-item">
-                    <button className="navbar-link" data-nav-link>Blog</button>
+                    <Link href="/blog">
+                        <button className={`navbar-link ${pathname == '/blog' && 'active'}`}>Blog</button>
+                    </Link>
                 </li>
                 <li className="navbar-item">
-                    <button className="navbar-link" data-nav-link>Contact</button>
+                    <Link href="/contact">
+                        <button className={`navbar-link ${pathname == '/contact' && 'active'}`}>Contact</button>
+                    </Link>
                 </li>
             </ul>
         </nav>
