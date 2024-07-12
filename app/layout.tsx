cfbda620a471c07909c0a081754a29e906a82474
Taimoor Imran <taimoorimran9@gmail.com>
2024-07-12 15:52:50 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
