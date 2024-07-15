7bc49af434223790bf1957d56c7cc5823f70846c
Taimoor Imran <taimoorimran9@gmail.com>
2024-07-15 17:51:43 +0500

    Removed static code

--- components/about/description.tsx
+++ components/about/description.tsx
@@ -2,16 +2,17 @@ const Description = () => {
   return (
     <section className="about-text">
       <p>
-        It&apos;m Creative Director and UI/UX Designer from Sydney, Australia,
-        working in web development and print media. I enjoy turning complex
-        problems into simple, beautiful and intuitive designs.
+        Senior Frontend Engineer with over 7 years of experience in designing
+        and developing high-performing web applications using Next.js, React,
+        and TypeScript. I specialize in transforming complex requirements into
+        user-friendly, scalable solutions that enhance user experience and
+        performance. My expertise extends across the full development lifecycle,
+        from initial concept to deployment and maintenance.
       </p>
       <p>
-        My job is to build your website so that it is functional and
-        user-friendly but at the same time attractive. Moreover, I add personal
-        touch to your product and make sure that is eye-catching and easy to
-        use. My aim is to bring across your message and identity in the most
-        creative way. I created web design for many famous brand companies.
+        I have a proven track record of modernizing legacy systems and
+        integrating cutting-edge technologies to drive innovation and
+        efficiency.
       </p>
     </section>
   );
