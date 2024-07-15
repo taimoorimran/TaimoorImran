b281fea3286e5d8a48e6e2e7cb1487b0bf1e65b6
Taimoor Imran <taimoorimran9@gmail.com>
2024-07-15 16:42:20 +0500

    Create clients.tsx

---
+++ components/about/clients.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+
+const Clients = () => {
+  return (
+    <section className="clients">
+      <h3 className="h3 clients-title">Clients</h3>
+      <ul className="clients-list has-scrollbar">
+        <li className="clients-item">
+          <a href="#">
+            <Image
+              src="/images/stock/logo-1-color.png"
+              width="100"
+              height="100"
+              alt="client logo"
+            />
+          </a>
+        </li>
+        <li className="clients-item">
+          <a href="#">
+            <Image
+              src="/images/stock/logo-2-color.png"
+              width="100"
+              height="100"
+              alt="client logo"
+            />
+          </a>
+        </li>
+        <li className="clients-item">
+          <a href="#">
+            <Image
+              src="/images/stock/logo-3-color.png"
+              width="100"
+              height="100"
+              alt="client logo"
+            />
+          </a>
+        </li>
+        <li className="clients-item">
+          <a href="#">
+            <Image
+              src="/images/stock/logo-4-color.png"
+              width="100"
+              height="100"
+              alt="client logo"
+            />
+          </a>
+        </li>
+        <li className="clients-item">
+          <a href="#">
+            <Image
+              src="/images/stock/logo-5-color.png"
+              width="100"
+              height="100"
+              alt="client logo"
+            />
+          </a>
+        </li>
+        <li className="clients-item">
+          <a href="#">
+            <Image
+              src="/images/stock/logo-6-color.png"
+              width="100"
+              height="100"
+              alt="client logo"
+            />
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+};
