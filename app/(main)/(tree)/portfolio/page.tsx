99fb987b251b7c4d5b7ad3c64e6f8a8ab522d75e
Taimoor Imran <taimoorimran9@gmail.com>
2024-07-17 00:25:55 +0500

    Update page.tsx

--- app/(main)/(tree)/portfolio/page.tsx
+++ app/(main)/(tree)/portfolio/page.tsx
@@ -1,5 +1,22 @@
 import PortfolioItem from "@/components/portfolio/items";
+import { ProjectItemProps } from "@/lib/interfaces";
 const Page = () => {
+  const projectItems: ProjectItemProps[] = [
+    {
+      name: "Al-Mukhtar",
+      category: "Web Development",
+    },
+    {
+      name: "Lone Wolf",
+      category: "Web Development",
+      imgSrc: "/images/stock/project-2.png",
+    },
+    {
+      name: "Retirement Analyzer",
+      category: "Web Development",
+      imgSrc: "/images/stock/project-2.png",
+    },
+  ];
   return (
     <article
       className="portfolio bg-eerie-white-2 dark:bg-eerie-black-2"
@@ -9,53 +26,23 @@ const Page = () => {
         <h2 className="h2 article-title">Portfolio</h2>
       </header>
       <section className="projects">
-        <ul className="filter-list">
-          <li className="filter-item">
-            <button className="active" data-filter-btn>
-              All
-            </button>
-          </li>
-          <li className="filter-item">
-            <button data-filter-btn>Web design</button>
-          </li>
-          <li className="filter-item">
-            <button data-filter-btn>Applications</button>
-          </li>
-          <li className="filter-item">
-            <button data-filter-btn>Web development</button>
-          </li>
-        </ul>
-        <div className="filter-select-box">
-          <button className="filter-select" data-select>
-            <div className="select-value" data-selecct-value>
-              Select category
-            </div>
-            <div className="select-icon">
-              {/* <ion-icon name="chevron-down"></ion-icon> */}
-            </div>
-          </button>
-          <ul className="select-list">
-            <li className="select-item">
-              <button data-select-item>All</button>
-            </li>
-            <li className="select-item">
-              <button data-select-item>Web design</button>
-            </li>
-            <li className="select-item">
-              <button data-select-item>Applications</button>
-            </li>
-            <li className="select-item">
-              <button data-select-item>Web development</button>
-            </li>
-          </ul>
-        </div>
         <ul className="project-list">
-          <PortfolioItem name="Finance" category="Web development" />
-          <PortfolioItem
-            name="Finance"
-            category="Web development"
-            imgSrc="/images/stock/project-2.png"
-          />
+          {projectItems.map(
+            (
+              { name, category, imgSrc, description }: ProjectItemProps,
+              key
+            ) => {
+              return (
+                <PortfolioItem
+                  key={`item_${name}_${key}`}
+                  name={name}
+                  category={category}
+                  imgSrc={imgSrc}
+                  description={description}
+                />
+              );
+            }
+          )}
         </ul>
       </section>
     </article>
