12a9a312808bdb9f7d274ba3feee1910e5fc3ab3
Taimoor Imran <taimoorimran9@gmail.com>
2024-07-16 14:13:26 +0500

    Update clients.tsx

--- components/about/clients.tsx
+++ components/about/clients.tsx
@@ -1,71 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
-
+interface ClientItemProps {
+  imgSrc?: string;
+  link?: string;
+}
+const ClientItem = ({ imgSrc, link }: ClientItemProps) => {
+  return (
+    <li className="clients-item">
+      <Link href={link ? link : "#"}>
+        <Image
+          src={imgSrc ? imgSrc : "/images/stock/logo-1-color.png"}
+          width="100"
+          height="100"
+          alt="client logo"
+        />
+      </Link>
+    </li>
+  );
+};
 const Clients = () => {
+  const clientItems = [{}, {}, {}, {}, {}, {}];
   return (
     <section className="clients">
       <h3 className="h3 clients-title">Clients</h3>
       <ul className="clients-list has-scrollbar">
-        <li className="clients-item">
-          <Link href="#">
-            <Image
-              src="/images/stock/logo-1-color.png"
-              width="100"
-              height="100"
-              alt="client logo"
-            />
-          </Link>
-        </li>
-        <li className="clients-item">
-          <Link href="#">
-            <Image
-              src="/images/stock/logo-2-color.png"
-              width="100"
-              height="100"
-              alt="client logo"
-            />
-          </Link>
-        </li>
-        <li className="clients-item">
-          <Link href="#">
-            <Image
-              src="/images/stock/logo-3-color.png"
-              width="100"
-              height="100"
-              alt="client logo"
-            />
-          </Link>
-        </li>
-        <li className="clients-item">
-          <Link href="#">
-            <Image
-              src="/images/stock/logo-4-color.png"
-              width="100"
-              height="100"
-              alt="client logo"
-            />
-          </Link>
-        </li>
-        <li className="clients-item">
-          <Link href="#">
-            <Image
-              src="/images/stock/logo-5-color.png"
-              width="100"
-              height="100"
-              alt="client logo"
-            />
-          </Link>
-        </li>
-        <li className="clients-item">
-          <Link href="#">
-            <Image
-              src="/images/stock/logo-6-color.png"
-              width="100"
-              height="100"
-              alt="client logo"
-            />
-          </Link>
-        </li>
+        {clientItems.map(({ imgSrc, link }: ClientItemProps, key) => {
+          return (
+            <ClientItem key={`client_${key}`} imgSrc={imgSrc} link={link} />
+          );
+        })}
       </ul>
     </section>
   );
