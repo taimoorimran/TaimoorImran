6724756096f612ad6038c301656151111cda1703
Taimoor Imran <taimoorimran9@gmail.com>
2025-02-05 15:21:38 +0500

    Update description.tsx

--- components/about/description.tsx
+++ components/about/description.tsx
@@ -2,8 +2,13 @@ const Description = () => {
   return (
     <section className="about-text">
       <p>
-        Results-driven Full Stack Engineer with 8+ years of experience designing, developing, and optimizing enterprise-level applications. Strong expertise in RESTful APIs, database management, workflow automation, debugging, and performance optimization. Experienced in SLA-driven troubleshooting, SDLC processes, and enterprise software architecture. Adept at leading development teams and integrating scalable backend solutions.
-
+        Results-driven Full Stack Engineer with 8+ years of experience
+        designing, developing, and optimizing enterprise-level applications.
+        Strong expertise in RESTful APIs, database management, workflow
+        automation, debugging, and performance optimization. Experienced in
+        SLA-driven troubleshooting, SDLC processes, and enterprise software
+        architecture. Adept at leading development teams and integrating
+        scalable backend solutions.
       </p>
     </section>
   );
